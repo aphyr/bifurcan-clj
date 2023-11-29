de98b0960fe5ce5ed237e38ae9b508d4b621a543
Aphyr <aphyr@aphyr.com>
2023-11-29 17:10:34 -0600

    Bifurcan 0.2.0-alpha7

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/aphyr/bifurcan-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[io.lacuna/bifurcan "0.2.0-alpha4"]]
+  :dependencies [[io.lacuna/bifurcan "0.2.0-alpha7"]]
   :repl-options {:init-ns bifurcan-clj.core}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]]}}
   :test-selectors {:buggy :buggy
